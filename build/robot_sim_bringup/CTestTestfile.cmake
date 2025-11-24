5ee5376c008092daa914d0d6dc6bffaee8e7b763
lukelpg <20lapg1@queensu.ca>
2025-11-24 14:04:38 -0500

    Wokring SLAM on robot

--- build/robot_sim_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luke/Projects/capstone/search-support-robots/ros-packages/robot_sim_bringup
-# Build directory: /home/luke/Projects/capstone/search-support-robots/ros-packages/build/robot_sim_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/robot_sim_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luke/Projects/capstone/search-support-robots/ros-packages/robot_sim_description
-# Build directory: /home/luke/Projects/capstone/search-support-robots/ros-packages/build/robot_sim_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
