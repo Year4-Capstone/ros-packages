98a3eb7bc5c037785a1ca95c89e67534b8b4fafa
lukelpg <20lapg1@queensu.ca>
2025-11-23 21:19:57 -0500

    Lidar and imu to urdf

---
+++ build/robot_sim_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/luke/Projects/capstone/search-support-robots/ros-packages/install/robot_sim_description/share/robot_sim_description/package.sh"
