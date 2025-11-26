77dde9f190472db9e5925b66bb9f46f2d7f2d621
callumberry <callumberry281@gmail.com>
2025-11-26 00:10:55 -0500

    Lidar working with og robot

--- robot_description/launch/view_robot_gazebo.launch.py
+++ robot_description/launch/view_robot_gazebo.launch.py
@@ -67,6 +67,8 @@ def generate_launch_description():
         executable='parameter_bridge',
         arguments=[
             '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU',
+            '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan' 
         ],
         output='screen'
     )
